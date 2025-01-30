8f84ad7d1e2355acdfa43dbd8f1393f2025b50b4
ankudinovaD <dsovenko@gmail.com>
2025-01-30 13:28:38 +0200

    addition arithmetic operation

--- 02/02.2_var1.py
+++ 02/02.2_var1.py
@@ -6,7 +6,11 @@ div_second, mode_second_number = divmod(mode_first_number, 1000)
 div_third, mode_third_number = divmod(mode_second_number, 100)
 div_fourth, mode_fourth_number = divmod(mode_third_number, 10)
 div_fifth, mode_fifth_number = divmod(mode_fourth_number, 1)
+
 # print (input_number)
-print(div_fifth, div_fourth, div_third, div_second, div_first)
+
+result = div_fifth * 10000 + div_fourth * 1000 + div_third * 100 + div_second * 10 + div_first
+
+print(result)
 
 # або можна просто прописати print (mode_third_number) і не викон. зайву дію
--- 02/02.2_var2.py
+++ 02/02.2_var2.py
@@ -1,11 +1,13 @@
 input_number = int(input('Input your number: '))
 
 
-first_number = input_number // 10000
-second_number = (input_number // 1000) % 10
-third_number = (input_number // 100) % 10
-fourth_number = (input_number // 10) % 10
-fifth_number = (input_number // 1) % 10
+first = input_number // 10000
+second = (input_number // 1000) % 10
+third = (input_number // 100) % 10
+fourth = (input_number // 10) % 10
+fifth = (input_number // 1) % 10
 
 # print (input_number)
-print(fifth_number, fourth_number, third_number, second_number, first_number)
+result = fifth * 10000 + fourth * 1000 + third * 100 + second * 10 + first
+
+print(result)
