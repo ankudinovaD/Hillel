fbed756217905c86095e5c6b2a30da50f5cbb102
ankudinovaD <dsovenko@gmail.com>
2025-02-10 23:37:07 +0200

    homework

---
+++ 05/homework_5.1.py
@@ -0,0 +1,21 @@
+import keyword
+import string
+
+input_string = input("Введіть ім'я змінної: ")
+
+if input_string[0].isdigit():
+    print(False)
+elif " " in input_string:
+    print(False)
+elif input_string == "_":
+    print(False)
+elif any(char in string.punctuation and char != "_" for char in input_string):
+    print(False)
+elif input_string.count('_') > 1:
+    print(False)
+elif keyword.iskeyword(input_string):
+    print(False)
+elif not input_string.islower():
+    print(False)
+else:
+    print(True)
