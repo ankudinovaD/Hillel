c62d303313103f0ccad944965d2b897329cabd11
ankudinovaD <dsovenko@gmail.com>
2025-02-12 13:39:31 +0200

    homework update

--- 05/homework_5.2.py
+++ 05/homework_5.2.py
@@ -1,26 +1,22 @@
-choice_client = 'y'
+while True:
+    first_number = int(input('Input your first number: '))
+    second_number = int(input('Input your second number: '))
+    arithmetic_operation = input('Input your arithmetic operation: ')
 
-while choice_client == 'y':
-    if choice_client == 'y':
-        first_number = int(input('Input your first number: '))
-        second_number = int(input('Input your second number: '))
-        arithmetic_operation = input('Input your arithmetic operation: ')
-        if arithmetic_operation == '*':
-            print(first_number * second_number)
-            choice_client = input('Do you want to continue?(y/n): ')
-        elif arithmetic_operation == '/':
-            if second_number == 0:
-                print('Error: Division by zero.')
-                choice_client = input('Do you want to continue?(y/n): ')
-            else:
-                print(first_number / second_number)
-                choice_client = input('Do you want to continue?(y/n): ')
-        elif arithmetic_operation == '+':
-            print(first_number + second_number)
-            choice_client = input('Do you want to continue?(y/n): ')
-        elif arithmetic_operation == '-':
-            print(first_number - second_number)
-            choice_client = input('Do you want to continue?(y/n): ')
+    if arithmetic_operation == '*':
+        print(first_number * second_number)
+    elif arithmetic_operation == '/':
+        if second_number == 0:
+            print('Error: Division by zero.')
         else:
-            print('Arithmetic operation not recognized.')
-            choice_client = input('Do you want to continue?(y/n): ')
+            print(first_number / second_number)
+    elif arithmetic_operation == '+':
+        print(first_number + second_number)
+    elif arithmetic_operation == '-':
+        print(first_number - second_number)
+    else:
+        print('Arithmetic operation not recognized.')
+
+    choice_client = input('Do you want to continue? (y/n): ').lower()
+    if choice_client not in ['y', 'yes']:
+        break
