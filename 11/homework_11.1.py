7104bb4b0ee4096bceb46a9d2e9422c7fabdb738
ankudinovaD <dsovenko@gmail.com>
2025-03-05 13:39:22 +0200

    homework

---
+++ 11/homework_11.1.py
@@ -0,0 +1,21 @@
+import math
+from inspect import isgenerator
+
+
+def prime_generator(end):
+
+    num = 2
+    while num <= end:
+        if all(num % i != 0 for i in range(2, int(math.sqrt(num)) + 1)):
+            yield num
+        num += 1
+
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+assert list(prime_generator(0)) == [], 'Test4'
+assert list(prime_generator(1)) == [], 'Test5'
+print('Ok')
