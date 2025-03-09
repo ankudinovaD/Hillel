66c18f7ba9c9f45434915c2cf817d63fb2d86c22
ankudinovaD <dsovenko@gmail.com>
2025-03-09 15:00:19 +0200

    homework

---
+++ 12/homework_12.1.py
@@ -0,0 +1,32 @@
+import codecs
+
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', encoding='utf-8') as infile:
+        cleaned_lines = []
+        for line in infile:
+            cleaned_line = ''
+            inside_tag = False
+            i = 0
+            while i < len(line):
+                if line[i] == '<':
+                    inside_tag = True
+                elif line[i] == '>' and inside_tag:
+                    inside_tag = False
+                elif not inside_tag:
+                    cleaned_line += line[i]
+                i += 1
+
+            cleaned_str = cleaned_line.strip()
+
+            if cleaned_str and any(c.isalpha() for c in cleaned_str) and "=" not in cleaned_str:
+                cleaned_lines.append(cleaned_str)
+
+    with codecs.open(result_file, 'w', encoding='utf-8') as outfile:
+        outfile.write('\n'.join(cleaned_lines) + '\n')
+
+    with codecs.open(result_file, 'r', encoding='utf-8') as result:
+        print(result.read())
+
+
+delete_html_tags('draft.html')
