88b4a6f9f0e74978280e5048c7533fdba4c1b43e
ankudinovaD <dsovenko@gmail.com>
2025-03-20 18:34:08 +0200

    homework

--- 13/homework_13.1.py
+++ 13/homework_13.1.py
@@ -27,7 +27,11 @@ class Group:
         self.group = set()
 
     def add_student(self, student):
+
+        if len(self.group) >= 10:
+            raise ValueError("В групі не може бути більше 10 студентів.")
         self.group.add(student)
+
 
     def delete_student(self, last_name):
         self.group.discard(self.find_student(last_name))
@@ -43,16 +47,35 @@ class Group:
 
 
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
-st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st2 = Student('Female', 25, 'Name1', 'LastName1', 'AN145')
+st3 = Student('Male', 25, 'Name2', 'LastName2', 'AN145')
+st4 = Student('Female', 25, 'Name3', 'LastName3', 'AN145')
+st5 = Student('Male', 25, 'Name4', 'LastName4', 'AN145')
+st6 = Student('Female', 25, 'Name5', 'LastName5', 'AN145')
+st7 = Student('Male', 25, 'Name6', 'LastName6', 'AN145')
+st8 = Student('Female', 25, 'Name7', 'LastName7', 'AN145')
+st9 = Student('Male', 25, 'Name8', 'LastName8', 'AN145')
+st10 = Student('Female', 25, 'Name9', 'LastName9', 'AN145')
+st11 = Student('Female', 25, 'Name10', 'LastName10', 'AN145')
+
 gr = Group('PD1')
 gr.add_student(st1)
 gr.add_student(st2)
+gr.add_student(st3)
+gr.add_student(st4)
+gr.add_student(st5)
+gr.add_student(st6)
+gr.add_student(st7)
+gr.add_student(st8)
+gr.add_student(st9)
+gr.add_student(st10)
+gr.add_student(st11)
 print(gr)
 
-assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
-assert gr.find_student('Jobs2') is None, 'Test2'
-assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
-
-gr.delete_student('Taylor')
-print(gr)  # Only one student
-gr.delete_student('Taylor')  # No error!
+# assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+# assert gr.find_student('Jobs2') is None, 'Test2'
+# assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+#
+# gr.delete_student('Taylor')
+# print(gr)  # Only one student
+# gr.delete_student('Taylor')  # No error!
